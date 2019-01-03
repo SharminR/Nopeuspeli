74300ad660eb8fffee44069adcf888c159a9c1cb
Rahman Sharmin <s1800091@student.businesscollege.fi>
2019-01-03 22:42:20 +0200

    Updates

---
+++ precache-manifest.949dd42e947de6f822d6dea8676ca1cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02dbdb20ad08aecd4b85",
+    "url": "/Nopeuspeli/static/js/runtime~main.02dbdb20.js"
+  },
+  {
+    "revision": "b1f856bb358895383b6f",
+    "url": "/Nopeuspeli/static/js/main.b1f856bb.chunk.js"
+  },
+  {
+    "revision": "f4bb04f8a4ae323e35e0",
+    "url": "/Nopeuspeli/static/js/1.f4bb04f8.chunk.js"
+  },
+  {
+    "revision": "b1f856bb358895383b6f",
+    "url": "/Nopeuspeli/static/css/main.7dd9186e.chunk.css"
+  },
+  {
+    "revision": "23d1664abffe9d7046c4a3ce60e07b9c",
+    "url": "/Nopeuspeli/index.html"
+  }
+];
